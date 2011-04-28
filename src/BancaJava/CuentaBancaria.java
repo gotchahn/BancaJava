589ae52f20edc40158f76083b5bcb110bd76650f
Carlos Gochez <cgotcha@gmail.com>
2011-04-27 22:31:49 -0600

    primera creacion del proyecto

---
+++ src/BancaJava/CuentaBancaria.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Bancajava;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Carlos Gochez
+ */
+public class CuentaBancaria {
+    protected int numCuenta;
+    protected Date apertura;
+    protected double tasa;
+    protected double saldo;
+
+    public CuentaBancaria(int nc,double t){
+        numCuenta = nc;
+        tasa = t;
+        saldo = 0;
+        apertura = new Date();
+    }
+
+    public void setTasa(double t){
+        tasa = t;
+    }
+
+    public int getNumeroCuenta(){
+        return numCuenta;
+    }
+
+    public double getTasa(){
+        return tasa;
+    }
+
+    public Date getApertura(){
+        return apertura;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void deposito(double monto){
+        saldo += monto;
+    }
+
+    public void imprimir(){
+        System.out.printf("El numero de cuenta es %d y tiene un saldo de %f \n",
+                numCuenta,saldo);
+        System.out.println("La Fecha de apertura es: " + apertura.toString());
+    }
+}
+
+
+
+
+
+
---
+++ src/BancaJava/CuentaPersonal.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Bancajava;
+
+/**
+ *
+ * @author Carlos Gochez
+ */
+public class CuentaPersonal extends CuentaBancaria{
+    private boolean activo;
+    public static double TASAPERSONAL = 0.08;
+
+    public CuentaPersonal(int nc){
+        super(nc,TASAPERSONAL);
+        activo = true;
+    }
+
+    /**
+     * Esta funcion deposita un monto dentro del saldo
+     * si la cuenta esta activa simplemente se acumula el monto,
+     * si la cuenta esta inactiva se re activa y se cobra un 5% sobre
+     * el monto depositado por conceptos de gastos de re-activacion
+     * @param m -> Monto a depositar
+     */
+    @Override
+    public void deposito(double m){
+        if( !activo ){
+            activo = true;
+            m = m * 0.95;
+        }
+        saldo += m;
+    }
+}
